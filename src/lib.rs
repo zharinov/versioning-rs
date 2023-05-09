f0ee8ee3696fa5fa03a402fe720f5ac01c90516e
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-05-09 14:32:41 +0300

    feat: Intermediate format for ranges

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,97 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, PartialEq, Eq)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Edge {
+    Hole,
+    Point,
+    Neighborhood(Direction),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Interval<V>
+where
+    V: Ord,
+{
+    position: V,
+    direction: Direction,
+    edge: Edge,
+}
+
+impl<V> Interval<V>
+where
+    V: Ord,
+{
+    pub fn new(position: V, direction: Direction, edge: Edge) -> Self {
+        Self {
+            position,
+            direction,
+            edge,
+        }
+    }
+
+    pub fn dedupe_union<'a>(x: &'a Self, y: &'a Self) -> Option<&'a Self> {
+        use Direction::*;
+        use Edge::*;
+
+        if x.direction != y.direction {
+            return None;
+        }
+
+        let direction = &x.direction;
+
+        if x.position < y.position {
+            return match direction {
+                Right => Some(x),
+                Left => Some(y),
+            };
+        }
+
+        if y.position < x.position {
+            return match direction {
+                Right => Some(y),
+                Left => Some(x),
+            };
+        }
+
+        return match (&x.edge, &y.edge) {
+            (Hole, Hole) => Some(x),
+            (Point, Point) => Some(x),
+
+            (Hole, Point) => Some(y),
+            (Point, Hole) => Some(x),
+
+            (Hole | Point, Neighborhood(dy)) => {
+                if dy == direction {
+                    Some(x)
+                } else {
+                    Some(y)
+                }
+            }
+            (Neighborhood(dx), Hole | Point) => {
+                if dx == direction {
+                    Some(y)
+                } else {
+                    Some(x)
+                }
+            }
+
+            (Neighborhood(dx), Neighborhood(dy)) => {
+                if dx == dy {
+                    return Some(x);
+                }
+
+                if dy == direction {
+                    Some(x)
+                } else {
+                    Some(y)
+                }
+            }
+        };
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +99,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn dedupe_union() {
+        let x = Interval::new(0, Direction::Right, Edge::Hole);
+        let y = Interval::new(0, Direction::Right, Edge::Point);
+
+        let res = Interval::dedupe_union(&x, &y);
+
+        assert_eq!(res, Some(&y));
     }
 }
